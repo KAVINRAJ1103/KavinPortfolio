8432c6d6adca3053a89599e8f914668092b8942e
KAVINRAJ1103 <tnkavinrajn17@gmail.com>
2023-08-10 18:31:52 +0530

    first commit

--- forms/contact.php
+++ forms/contact.php
@@ -7,8 +7,9 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
+  $receiving_email_address = 'kavinrajnalarajan@gmail.com';
+  echo "run";
+ windows.alert("check1");
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
